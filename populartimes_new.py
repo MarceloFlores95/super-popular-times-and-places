a609511c3c6779f5f9bf3c1daaecde2a108422f0
MarceloFlores95 <luismarcelofc@gmail.com>
2022-02-06 03:55:26 -0600

    Clear code

--- populartimes_new.py
+++ populartimes_new.py
@@ -8,16 +8,20 @@ import time
 import os, glob
 import csv
 
+'''
+Ejemplo de doc
+'''
+
 ### Dejar prendida variable single_coord para obtener los lugares cercanos de una ubicacion ###
 single_coord = False
 ### Dejar prendida la variable popular_times para obtener informacion sobre trafico de personas en el lugar durante el dia ###
-porpular_times = False
+porpular_times = True
 ### Obtiene toda la información de un solo lugar
-single_place_info = True
+single_place_info = False
 
 ### Nombre del archivo y carpeta de salida ###
-file = "ejemplo" # De preferencia el nombre del lugar
-carpeta = "Brasil-ejemplo" # Carpeta donde se guardaran todos los json obtenidos.
+file = "PopularTimesLuztopía" # De preferencia el nombre del lugar
+carpeta = "PopularTimesLuztopía" # Carpeta donde se guardaran todos los json obtenidos.
 
 ### Si no existe el nombre de la carpeta, se crea. ###
 if not os.path.exists(carpeta):
@@ -25,11 +29,11 @@ if not os.path.exists(carpeta):
 
 # Requiere
 radius = 2000 # Metros
-loc = (-23.550041,-46.5969827) ### Latitud y longitud del lugar a examinar. ###
+loc = (20.59288318381712, -100.40927576975132) ### Latitud y longitud del lugar a examinar. ###
 # Optional
 keyword = ''
 rankby = ''
-single_place_id = 'ChIJN-5quXSyKIQR2a3xkHf5bTo'
+single_place_id = [] # Example 'ChIJ4_j1to5tVo8RQBjYcPA_oXY'
 
 
 '''La busqueda sale mejor si se usan types ya que puede ignorar ciertos negocios aunque esten a lado
@@ -43,8 +47,9 @@ single_place_id = 'ChIJN-5quXSyKIQR2a3xkHf5bTo'
     https://developers.google.com/places/web-service/supported_types?hl=es-419
 '''
 
-### Tipos de locales que se quieren buscar con el populartimes.py ### 
-types = ["restaurant"]
+### Tipos de locales que se quieren buscar con el populartimes.py ###
+types = ["atm", "bakery" ,"bank" ,"bar" ,"clothing_store" ,"department_store","drugstore" ,"grocery_or_supermarket", "hospital","park" ,"parking","pharmacy" ,"restaurant" ,"shopping_mall" ,"store" ,"supermarket"]
+### Ejemplos ###
 #types = ["accounting", "airport", "amusement_park" ,"aquarium", "art_gallery", "atm", "bakery" ,"bank" ,"bar" ,"beauty_salon" ,"bicycle_store" ,"book_store" ,"bowling_alley" ,"bus_station" ,"cafe" ,"campground" ,"car_dealer" ,"car_rental" ,"car_repair" ,"car_wash" ,"casino" ,"cemetery" ,"church" ,"city_hall" ,"clothing_store" ,"convenience_store" ,"courthouse" ,"dentist" ,"department_store" ,"doctor" ,"drugstore" ,"electrician" ,"electronics_store" ,"embassy" ,"fire_station" ,"florist" ,"funeral_home" ,"furniture_store" ,"gas_station" ,"grocery_or_supermarket" ,"gym" ,"hair_care" ,"hardware_store" ,"hindu_temple" ,"home_goods_store" ,"hospital" ,"insurance_agency" ,"jewelry_store","laundry" ,"lawyer" ,"library" ,"light_rail_station" ,"liquor_store" ,"local_government_office" ,"locksmith" ,"lodging" ,"meal_delivery" ,"meal_takeaway" ,"mosque" ,"movie_rental" ,"movie_theater" ,"moving_company" ,"museum" ,"night_club" ,"painter" ,"park" ,"parking" ,"pet_store" ,"pharmacy" ,"physiotherapist" ,"plumber" ,"pólice" ,"post_office" ,"primary_school" ,"real_estate_agency" ,"restaurant" ,"roofing_contractor" ,"rv_park" ,"school" ,"secondary_school" ,"shoe_store" ,"shopping_mall" ,"spa" ,"stadium" ,"storage" ,"store" ,"subway_station" ,"supermarket" ,"synagogue" ,"taxi_stand" ,"tourist_attraction" ,"train_station" ,"transit_station" ,"travel_agency" ,"university" ,"veterinary_care" ,"zoo"] # Todos los negocios
 
 s = pd.DataFrame() # Inicializar el dataframe
@@ -52,7 +57,7 @@ i = 0 # Manejo de la primera fila
 
 ### APIKEY para correr el script. "REQUERIDO"
 # APIKEY Datlas
-APIKEY = ""
+APIKEY = 'X'
 
 
 def findPlaces(loc=loc, radius=radius, pagetoken = None, file="data", type="", rankby="", keyword=""):
@@ -90,7 +95,6 @@ pagetoken = None
 ### Si single_coord esta prendido se hace una llamada al api de google places y trae info del lugar ###
 if (single_coord):
     for t in types:
-
         while True:
             pagetoken = findPlaces(loc=loc, radius=radius, pagetoken=pagetoken, file=file, type=t, rankby=rankby, keyword=keyword)
             time.sleep(2)
@@ -98,6 +102,14 @@ if (single_coord):
             if not pagetoken:
                 i = 0
                 break
+    
+    path = './'+carpeta+'/'
+    all_files = glob.glob(os.path.join(path, "*.csv"))
+    df_from_each_file = (pd.read_csv(f, sep=',') for f in all_files)
+    df_merged = pd.concat(df_from_each_file, ignore_index=True)
+    df_merged.drop_duplicates(subset="id", keep = 'first', inplace = True) 
+    df_merged.to_csv( './'+carpeta+'/'+file+"-merged.csv", index=False)
+
     
 ### OBTIENE LOS POPULARTIMES DEL ID SELECCIONADO ###
 ### Actualizar a que lea cada archivo ###
@@ -292,17 +304,40 @@ if (porpular_times):
 
 
 if (single_place_info):
-    url = "https://maps.googleapis.com/maps/api/place/details/json?placeid={PLACE_ID}&key={APIKEY}".format(PLACE_ID = single_place_id, APIKEY = APIKEY)
-    response = requests.get(url)
-    res = json.loads(response.text)
+    column_names = ["lat", "lon", "id","nombre", "direccion", "tipo", "rating"]
+    #csv_places = pd.read_csv('./'+carpeta+'/'+file+"-merged.csv", names=column_names)
+    csv_places = pd.read_csv('./'+carpeta+ 'single_file'+'/'+'5km_CheckData_.csv', names=column_names)
+    places_id_list= csv_places.id.tolist()
+    places_id_list.pop(0)
+    single_place_id = places_id_list
+    for spi in single_place_id:
+        url = "https://maps.googleapis.com/maps/api/place/details/json?placeid={PLACE_ID}&key={APIKEY}".format(PLACE_ID = spi, APIKEY = APIKEY)
+        print(url)
+        response = requests.get(url)
+        res = json.loads(response.text)
+        if 'formatted_phone_number' not in res['result']:
+            res["result"]["formatted_phone_number"] = "None"
+        
+        if 'user_ratings_total' not in res['result']:
+            res["result"]["user_ratings_total"] = "None"
 
-    info = [res["result"]["name"],res["result"]["geometry"]["location"]["lat"],res["result"]["geometry"]["location"]["lng"],res["result"]["place_id"],res["result"]["types"],res["result"]["url"],res["result"]["user_ratings_total"],res["result"]["formatted_address"], res["result"]["formatted_phone_number"]]
-    
-    with open('single_file'+file+".csv", 'a') as c:
-        writer = csv.writer(c)
-        writer.writerow(["name","lat","lon","id", "tipo","url","rating","direccion","telefono"])
-        writer.writerow(info)
-    c.close()
+        info = [res["result"]["name"],res["result"]["geometry"]["location"]["lat"],res["result"]["geometry"]["location"]["lng"],res["result"]["place_id"],res["result"]["types"],res["result"]["url"],res["result"]["user_ratings_total"],res["result"]["formatted_address"],res["result"]["formatted_phone_number"]]
 
+        if not os.path.exists(carpeta +'single_file'):
+            os.mkdir(carpeta+'single_file')
+
+        with open('./'+carpeta+'single_file/'+file+ spi+".csv", 'a') as c:
+            writer = csv.writer(c)
+            writer.writerow(["name","lat","lon","id", "tipo","url","rating","direccion","telefono"])
+            writer.writerow(info)
+        c.close()
+        time.sleep(2)
+
+    path = './'+carpeta+'single_file/'
+    all_files = glob.glob(os.path.join(path, "*.csv"))
+    df_from_each_file = (pd.read_csv(f, sep=',') for f in all_files)
+    df_merged = pd.concat(df_from_each_file, ignore_index=True)
+    df_merged.drop_duplicates(subset="id", keep = 'first', inplace = True) 
+    df_merged.to_csv( './'+carpeta+'single_file/'+file+"-merged.csv", index=False) 
 
 print("Listo!")
